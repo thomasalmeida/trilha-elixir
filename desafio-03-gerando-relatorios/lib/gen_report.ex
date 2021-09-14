3bb1552cd54362939ce6354426f7e6cf365bc94f
Thomas Almeida <thomasfa86@hotmail.com>
2021-09-14 01:11:21 -0300

    solution to challenge three

---
+++ desafio-03-gerando-relatorios/lib/gen_report.ex
@@ -0,0 +1,51 @@
+defmodule GenReport do
+  alias GenReport.Parser
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_template(), fn line, report -> sum_values(line, report) end)
+  end
+
+  def build, do: {:error, "Insira o nome de um arquivo"}
+
+  defp sum_values(
+         [name, hours, _day, month, year],
+         %{
+           "all_hours" => all_hours,
+           "hours_per_month" => hours_per_month,
+           "hours_per_year" => hours_per_year
+         } = report
+       ) do
+    all_hours = all_hours_sum(all_hours, name, hours)
+    hours_per_month = hours_per_month_sum(hours_per_month, name, month, hours)
+    hours_per_year = hours_per_year_sum(hours_per_year, name, hours, year)
+
+    report
+    |> Map.put("all_hours", all_hours)
+    |> Map.put("hours_per_month", hours_per_month)
+    |> Map.put("hours_per_year", hours_per_year)
+  end
+
+  defp hours_per_year_sum(hours_per_year, name, hours, year) do
+    years = hours_per_year[name] || %{}
+    years = Map.put(years, year, (years[year] || 0) + hours)
+
+    Map.put(hours_per_year, name, years)
+  end
+
+  defp hours_per_month_sum(hours_per_month, name, month, hours) do
+    months = hours_per_month[name] || %{}
+    months = Map.put(months, month, (months[month] || 0) + hours)
+
+    Map.put(hours_per_month, name, months)
+  end
+
+  defp all_hours_sum(all_hours, name, hours) do
+    Map.put(all_hours, name, (all_hours[name] || 0) + hours)
+  end
+
+  defp report_template do
+    %{"all_hours" => %{}, "hours_per_month" => %{}, "hours_per_year" => %{}}
+  end
+end
